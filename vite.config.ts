4acece740af0c97e084aaf022069eb1ff8d9b1a2
Ishan Madhusanka <ahtimadhusanka@gmail.com>
2024-07-13 13:24:52 +0530

    remove unwanted files for build

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	publicDir: false,
 	build: {
 		lib: {
 			entry: path.resolve(__dirname, "src/index.ts"),
